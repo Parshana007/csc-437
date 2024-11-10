2c269fd2dd0c88efbc297e6cc4ca63551316210f
Parshana007 <parshana007@gmail.com>
2024-11-09 22:29:53 -0800

    lab 11; finished put

--- packages/server/src/models/user.ts
+++ packages/server/src/models/user.ts
@@ -2,5 +2,4 @@ export interface User {
     name: string;
     contactInfo: string; /*Email format ending in .edu*/
     profilePic: string;
-    profileLink: string;
 }
--- packages/server/src/routes/users.ts
+++ packages/server/src/routes/users.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import Users from "../services/users-svc";
-import { UserPage } from "../pages/userPage";
 import { User } from "models";
 
 const router = express.Router();
@@ -11,11 +10,28 @@ router.get("/", (_, res: Response) => {
     .catch((err) => res.status(500).send(err));
 });
 
+router.post("/", (req: Request, res: Response) => {
+  const newUser = req.body;
+
+  Users.create(newUser)
+    .then((user: User) => res.status(201).json(user))
+    .catch((err) => res.status(500).send(err));
+});
+
 router.get("/:userName", (req: Request, res: Response) => {
   const { userName } = req.params;
   Users.get(userName)
     .then((user: User) => res.json(user))
     .catch((err) => res.status(404).send(err));
+});
+
+router.put("/:userName", (req: Request, res: Response) => {
+  const { userName } = req.params;
+  const newUser = req.body;
+
+  Users.update(userName, newUser)
+    .then((user: User) => res.json(user))
+    .catch((err) => res.status(404).end());
 });
 
 export default router;
